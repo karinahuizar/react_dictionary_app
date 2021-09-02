270565146fb46a7b1723b2960b6708818c7900a4
Karina Huizar <karinahuizar@gmail.com>
2021-09-01 21:09:54 -0500

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import dictionary from "./dictionary.png";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={dictionary} className="App-logo img-fluid" alt="logo" />
+      </header>
+      Hello World App-header
+    </div>
+  );
+}
