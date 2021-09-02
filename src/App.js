60a304668ac13ee12d8a366d82372e56cf6eca36
Karina Huizar <karinahuizar@gmail.com>
2021-09-01 21:54:57 -0500

    Created the Dictionary component. Added useState.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import dictionary from "./dictionary.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={dictionary} className="App-logo img-fluid" alt="logo" />
-      </header>
-      Hello World
+      <div className="container">
+        <header className="App-header">
+          <img src={dictionary} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          <small>Coded by Karina Huizar</small>
+        </footer>
+      </div>
     </div>
   );
 }
